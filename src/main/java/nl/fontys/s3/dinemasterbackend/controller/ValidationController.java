0252908f05d6a40c2ff1d3c49cd4fa3597f0358b
Samuil2004 <s.kozarov23@acsbg.org>
2025-01-18 21:53:45 +0100

    endpoint to test external api is addded

--- src/main/java/nl/fontys/s3/dinemasterbackend/controller/ValidationController.java
+++ src/main/java/nl/fontys/s3/dinemasterbackend/controller/ValidationController.java
@@ -8,8 +8,17 @@ import nl.fontys.s3.dinemasterbackend.business.dtos.get.ValidateAddressRequest;
 import nl.fontys.s3.dinemasterbackend.business.dtos.get.ValidateAddressResponse;
 import nl.fontys.s3.dinemasterbackend.business.order_use_cases.CalculateDeliveryFee;
 import nl.fontys.s3.dinemasterbackend.business.validation_services.ValidateAddress;
+import org.cloudinary.json.JSONArray;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/validation")
@@ -34,4 +43,42 @@ public class ValidationController {
         CalculateDeliveryFeeResponse response = calculateDeliveryFee.calculateDeliveryFee(validateAddressRequest);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/test-public-api")
+    public ResponseEntity<String> testPublicApi(@RequestBody @Valid ValidateAddressRequest request ) {
+        String fullAddress = String.format("%s, %s, %s, %s",
+                request.getStreet(), request.getCity(), request.getPostalCode(), request.getCountry());
+        String nominatimURL = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json&addressdetails=1",
+                UriUtils.encode(fullAddress, StandardCharsets.UTF_8));
+
+        try {
+            URL url = new URL(nominatimURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+
+            JSONArray jsonArray = new JSONArray(response.toString());
+            if (jsonArray.length() > 0) {
+                String lat = jsonArray.getJSONObject(0).getString("lat");
+                String lon = jsonArray.getJSONObject(0).getString("lon");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("TRUE");
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("False");
+        }
+         catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+        }
+    }
+
 }
