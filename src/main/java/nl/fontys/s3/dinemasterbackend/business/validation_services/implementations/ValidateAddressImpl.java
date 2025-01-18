26f2603c5761883add06aebb9838fd549bde980d
Samuil2004 <s.kozarov23@acsbg.org>
2025-01-18 23:34:06 +0100

    location provider changed

--- src/main/java/nl/fontys/s3/dinemasterbackend/business/validation_services/implementations/ValidateAddressImpl.java
+++ src/main/java/nl/fontys/s3/dinemasterbackend/business/validation_services/implementations/ValidateAddressImpl.java
@@ -1,5 +1,9 @@
 package nl.fontys.s3.dinemasterbackend.business.validation_services.implementations;
 
+import com.opencagedata.jopencage.JOpenCageGeocoder;
+import com.opencagedata.jopencage.model.JOpenCageForwardRequest;
+import com.opencagedata.jopencage.model.JOpenCageLatLng;
+import com.opencagedata.jopencage.model.JOpenCageResponse;
 import lombok.AllArgsConstructor;
 import nl.fontys.s3.dinemasterbackend.business.dtos.get.ValidateAddressRequest;
 import nl.fontys.s3.dinemasterbackend.business.dtos.get.ValidateAddressResponse;
@@ -20,38 +24,60 @@ public class ValidateAddressImpl implements ValidateAddress {
 
 
     @Override
-    public ValidateAddressResponse isAddressValid(ValidateAddressRequest request) {
-        String fullAddress = String.format("%s, %s, %s, %s",
-                request.getStreet(), request.getCity(), request.getPostalCode(), request.getCountry());
-        String nominatimURL = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json&addressdetails=1",
-                UriUtils.encode(fullAddress, StandardCharsets.UTF_8));
+    public ValidateAddressResponse isAddressValid(ValidateAddressRequest validationRequest) {
 
-        try {
-            URL url = new URL(nominatimURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("User-Agent", "DineMasterPro/1.0");
+        String address = validationRequest.getStreet() + ", "
+                + validationRequest.getCity() + ", "
+                + validationRequest.getPostalCode() + ", "
+                + validationRequest.getCountry();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
+        JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder("bbe24e79785a4c44b59bf417e2174cb0");
+        JOpenCageForwardRequest request = new JOpenCageForwardRequest(address);
+        //request.setRestrictToCountryCode("za"); // restrict results to a specific country
+        //request.setBounds(18.367, -34.109, 18.770, -33.704); // restrict results to a geographic bounding box (southWestLng, southWestLat, northEastLng, northEastLat)
 
+        JOpenCageResponse response = jOpenCageGeocoder.forward(request);
+        JOpenCageLatLng firstResultLatLng = response.getFirstPosition(); // get the coordinate pair of the first result
+        //System.out.println(firstResultLatLng.getLat().toString() + "," + firstResultLatLng.getLng().toString());
+        if(firstResultLatLng != null) {
+            return ValidateAddressResponse.builder().isValid(true).locationLatitude(firstResultLatLng.getLat().toString()).locationLongitude(firstResultLatLng.getLng().toString()).build();
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-
-            JSONArray jsonArray = new JSONArray(response.toString());
-            if (jsonArray.length() > 0) {
-                String lat = jsonArray.getJSONObject(0).getString("lat");
-                String lon = jsonArray.getJSONObject(0).getString("lon");
-                return ValidateAddressResponse.builder().isValid(true).locationLatitude(lat).locationLongitude(lon).build();
-            }
-            return ValidateAddressResponse.builder().isValid(false).build();
-        } catch (Exception e) {
-            return ValidateAddressResponse.builder().isValid(false).build();
         }
+            return ValidateAddressResponse.builder().isValid(false).build();
+
+        //String coordinates = firstResultLatLng.getLat().toString() + "," + firstResultLatLng.getLng().toString();
+
+//        String fullAddress = String.format("%s, %s, %s, %s",
+//                request.getStreet(), request.getCity(), request.getPostalCode(), request.getCountry());
+//        String nominatimURL = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json&addressdetails=1",
+//                UriUtils.encode(fullAddress, StandardCharsets.UTF_8));
+//
+//        try {
+//            URL url = new URL(nominatimURL);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("User-Agent", "DineMasterPro/1.0");
+//
+//            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String inputLine;
+//            StringBuilder response = new StringBuilder();
+//
+//
+//            while ((inputLine = in.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            in.close();
+//
+//
+//            JSONArray jsonArray = new JSONArray(response.toString());
+//            if (jsonArray.length() > 0) {
+//                String lat = jsonArray.getJSONObject(0).getString("lat");
+//                String lon = jsonArray.getJSONObject(0).getString("lon");
+//                return ValidateAddressResponse.builder().isValid(true).locationLatitude(lat).locationLongitude(lon).build();
+//            }
+//            return ValidateAddressResponse.builder().isValid(false).build();
+//        } catch (Exception e) {
+//            return ValidateAddressResponse.builder().isValid(false).build();
+//        }
     }
 }
--- src/main/java/nl/fontys/s3/dinemasterbackend/controller/ValidationController.java
+++ src/main/java/nl/fontys/s3/dinemasterbackend/controller/ValidationController.java
@@ -1,5 +1,9 @@
 package nl.fontys.s3.dinemasterbackend.controller;
 
+import com.opencagedata.jopencage.JOpenCageGeocoder;
+import com.opencagedata.jopencage.model.JOpenCageForwardRequest;
+import com.opencagedata.jopencage.model.JOpenCageLatLng;
+import com.opencagedata.jopencage.model.JOpenCageResponse;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -45,41 +49,65 @@ public class ValidationController {
     }
 
     @PostMapping("/test-public-api")
-    public ResponseEntity<String> testPublicApi(@RequestBody @Valid ValidateAddressRequest request ) {
-        String fullAddress = String.format("%s, %s, %s, %s",
-                request.getStreet(), request.getCity(), request.getPostalCode(), request.getCountry());
-        String nominatimURL = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json&addressdetails=1",
-                UriUtils.encode(fullAddress, StandardCharsets.UTF_8));
+    public ResponseEntity<String> testPublicApi(@RequestBody @Valid ValidateAddressRequest validationRequest ) {
 
-        try {
-            URL url = new URL(nominatimURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("User-Agent", "DineMasterPro/1.0");
+        String address = validationRequest.getStreet() + ", "
+                + validationRequest.getCity() + ", "
+                + validationRequest.getPostalCode() + ", "
+                + validationRequest.getCountry();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
+        JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder("bbe24e79785a4c44b59bf417e2174cb0");
+        JOpenCageForwardRequest request = new JOpenCageForwardRequest(address);
+        //request.setRestrictToCountryCode("za"); // restrict results to a specific country
+        //request.setBounds(18.367, -34.109, 18.770, -33.704); // restrict results to a geographic bounding box (southWestLng, southWestLat, northEastLng, northEastLat)
 
+        JOpenCageResponse response = jOpenCageGeocoder.forward(request);
+        JOpenCageLatLng firstResultLatLng = response.getFirstPosition(); // get the coordinate pair of the first result
+        System.out.println(firstResultLatLng.getLat().toString() + "," + firstResultLatLng.getLng().toString());
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
+        String coordinates = firstResultLatLng.getLat().toString() + "," + firstResultLatLng.getLng().toString();
+
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Coordinate"+ coordinates);
 
 
-            JSONArray jsonArray = new JSONArray(response.toString());
-            if (jsonArray.length() > 0) {
-                String lat = jsonArray.getJSONObject(0).getString("lat");
-                String lon = jsonArray.getJSONObject(0).getString("lon");
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("TRUE");
-            }
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("False");
-        }
-         catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
-        }
+//        String fullAddress = String.format("%s, %s, %s, %s",
+//                request.getStreet(), request.getCity(), request.getPostalCode(), request.getCountry());
+//        String nominatimURL = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json&addressdetails=1",
+//                UriUtils.encode(fullAddress, StandardCharsets.UTF_8));
+//
+//        try {
+//            URL url = new URL(nominatimURL);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("User-Agent", "DineMasterPro/1.0");
+//
+//            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String inputLine;
+//            StringBuilder response = new StringBuilder();
+//
+//
+//            while ((inputLine = in.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            in.close();
+//
+//
+//            JSONArray jsonArray = new JSONArray(response.toString());
+//            if (jsonArray.length() > 0) {
+//                String lat = jsonArray.getJSONObject(0).getString("lat");
+//                String lon = jsonArray.getJSONObject(0).getString("lon");
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("TRUE");
+//            }
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("False");
+//        }
+//         catch (Exception e) {
+//            e.printStackTrace();
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+//        }
+
+        //            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+
+
     }
 
 }
